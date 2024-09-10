6d1abaab6d6b681403b427e75a419b40d35faf01
Nofer <sxx261@case.edu>
2024-09-09 20:19:27 -0400

    Update assign00_02.ml

--- assigns/assign00/lib/assign00_02.ml
+++ assigns/assign00/lib/assign00_02.ml
@@ -1,19 +1,17 @@
+let sqrt (n : int) : int = 
+    let rec sqrt_rec (i : int) : int =
+        if i * i >= n then i
+        else sqrt_rec (i + 1)
+    in
+    sqrt_rec (0)
+
 let is_prime (n : int) : bool =
   if n < 2 then false
   else
-    let sqrt (n : int) : int = 
-      let rec sqrt_rec (i : int) : int =
-        if i * i >= n then i
-        else sqrt_rec (i + 1)
-      in
-      sqrt_rec (0)
-    in
-    let rng_max : int = sqrt (n)
-    in
-    let rng_max = if rng_max > n then n else rng_max
+    let rng_max : int = if n < 6 then n / 2 else sqrt (n)
     in
     let rec is_prime_rec (i : int) : bool =
-      if i >= rng_max then true
+      if i > rng_max then true
       else if n mod i = 0 then false
       else is_prime_rec (i + 1)
     in
